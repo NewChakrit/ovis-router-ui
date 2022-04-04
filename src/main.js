5f88fdf2b9b21ab185bc81ae255481a0c6387eba
DESKTOP-NOKITPQ\Admin <imnew108@gmail.com>
2022-04-04 21:01:58 +0700

    'save'

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import { createApp, useAttrs } from 'vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
+import generatedRoutes from 'virtual:generated-pages'
+import 'windi.css'
+
+import App from './App.vue'
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes: generatedRoutes,
+})
+
+createApp(App)
+  .use(router)
+  .mount('#app')
+
---
+++ src/pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="row">
+    <h1>{{ row }},{{ col }}</h1>
+    <div>Row = <input type="number" v-model.lazy.number="row" min="10" max="10" /></div>
+    <div>Col = <input type="number" v-model.lazy.number="col" /></div>
+
+    <div>
+      <div>Logo</div>
+    </div>
+    <div class="flex" v-for="c in col">
+      <div v-for="r in row">
+        <button>{{ (c - 1) * row + r }}</button>
+      </div>
+    </div>
+    <button>button2</button>
+    <div>preview</div>
+    <div>controls</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      row: 3,
+      col: 7,
+    };
+  },
+};
+</script>
