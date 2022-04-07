fff4abac6635cf17ce3dcc4bac04fc69b60539fd
NewChakrit <imnew108@gmail.com>
2022-04-07 08:39:07 -0700

    'save'

--- src/main.js
+++ src/main.js
@@ -1,16 +1,13 @@
-import { createApp, useAttrs } from 'vue'
-import { createRouter, createWebHashHistory } from 'vue-router'
-import generatedRoutes from 'virtual:generated-pages'
-import 'windi.css'
+import { createApp } from "vue";
+import { createRouter, createWebHashHistory } from "vue-router";
+import generatedRoutes from "virtual:generated-pages";
+import "windi.css";
 
-import App from './App.vue'
+import App from "./App.vue";
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes: generatedRoutes,
-})
+});
 
-createApp(App)
-  .use(router)
-  .mount('#app')
-
+createApp(App).use(router).mount("#app");
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,7 +3,7 @@
     <!-- Logo -->
 
     <div>
-      <div class="ml-6 mr-42">
+      <div class="ml-6 mx-42">
         <h2 class="font-medium text-white">
           Smart Videohub <span class="text-slate-400 font-thin">20 X 20</span>
         </h2>
@@ -12,42 +12,38 @@
 
     <!-- button 1 -->
 
-    <div class="flex mr-4">
-      <div v-for="c in col1">
-        <div class="flex justify-center" v-for="r in row">
-          <button
-            class="shadow-gray-900 bg-button w-9 my-2px mx-4px rounded-md border-gray-500 border-1 text-[10px] h-[30px]"
-          >
-            {{
-              r === 1
-                ? btn1Row1[c - 1]
-                : r === 2
-                ? btn1Row2[c - 1]
-                : btn1Row3[c - 1]
-            }}
-          </button>
-        </div>
+    <div
+      class="h-full flex flex-col justify-center flex-wrap w-[340px] shrink mr-[20px]"
+    >
+      <div
+        class="flex justify-center"
+        v-for="item in config.inputLadels"
+        :key="item"
+      >
+        <button
+          :class="{ 'bg-red-900': `${item - 1}` === activeNum }"
+          class="shadow-gray-900 bg-button w-9 my-2px mx-4px rounded-md border-gray-500 border-1 text-[10px] h-[30px]"
+          @click="onButtonClick(item.id)"
+        >
+          {{ item.id }}
+        </button>
       </div>
     </div>
 
     <!-- button 2 -->
 
-    <div class="flex mr-8">
-      <div v-for="c in col2">
-        <div v-for="r in row">
-          <button
-            class="bg-button w-9 my-2px mx-4px rounded-md border-gray-500 border-1 text-[10px] h-[30px]"
-          >
-            {{
-              r === 1
-                ? btn2Row1[c - 1]
-                : r === 2
-                ? btn2Row2[c - 1]
-                : btn2Row3[c - 1]
-            }}
-          </button>
-        </div>
-      </div>
+    <div
+      class="flex mr-8 flex-col flex-wrap shrink h-full w-[100px] items-center justify-center"
+    >
+      <button
+        v-for="item in modeList"
+        class="w-9 my-2px mx-4px rounded-md border-gray-500 border-1 text-[10px] h-[30px]"
+        :class="{ 'bg-amber-300': item.id === mode }"
+        @click="onModeClick(item.id)"
+        :key="item"
+      >
+        {{ item.text }}
+      </button>
     </div>
 
     <!-- preview -->
@@ -70,7 +66,7 @@
 
     <div class="mr-2">
       <h2 class="text-slate-300">
-        Blackmagic<span class="text-slate-400">design</span>
+        Whitemagic<span class="text-slate-400">design</span>
       </h2>
     </div>
 
@@ -85,40 +81,124 @@
 <script>
 export default {
   data() {
-    // btn1
-    const btn1Row1 = [1, 4, 7, 10, 13, 16, 19];
-    const btn1Row2 = [2, 5, 8, 11, 14, 17, 20];
-    const btn1Row3 = [3, 6, 9, 12, 15, 18];
-
-    // const btn1 = [
-    //   [1, 4, 7, 10, 13, 16, 19],
-    //   [2, 5, 8, 11, 14, 17, 20],
-    //   [3, 6, 9, 12, 15, 18],
-    // ];
-
-    // btn2
-    const btn2Row1 = ["SRC", "MENU"];
-    const btn2Row2 = ["DEST", "VIDEO"];
-    const btn2Row3 = ["CLEAR", "TAKE"];
-
-    // const btn2 = [
-    //   ["SRC", "MENU"],
-    //   ["DEST", "VIDEO"],
-    //   ["CLEAR", "TAKE"],
-    // ];
     return {
-      row: 3,
-      col1: 7,
-      col2: 2,
-      btn1Row1,
-      btn1Row2,
-      btn1Row3,
-      btn2Row1,
-      btn2Row2,
-      btn2Row3,
-      // btn1,
-      // btn2,
+      modeList: [
+        { id: "src", text: "SRC" },
+        { id: "dst", text: "DEST" },
+        { id: "cle", text: "CLR" },
+        { id: "menu", text: "MENU" },
+        { id: "video", text: "VIDEO" },
+        { id: "take", text: "TAKE" },
+      ],
+
+      config: {
+        inputLadels: [
+          { 0: "Input 1", id: "1" },
+          { 1: "Input 2", id: "2" },
+          { 2: "Input 3", id: "3" },
+          { 3: "Input 4", id: "4" },
+          { 4: "Input 5", id: "5" },
+          { 5: "Input 6", id: "6" },
+          { 6: "Input 7", id: "7" },
+          { 7: "Input 8", id: "8" },
+          { 8: "Input 9", id: "9" },
+          { 9: "Input 10", id: "10" },
+          { 10: "Input 11", id: "11" },
+          { 11: "Input 12", id: "12" },
+          { 12: "Input 13", id: "13" },
+          { 13: "Input 14", id: "14" },
+          { 14: "Input 15", id: "15" },
+          { 15: "Input 16", id: "16" },
+          { 16: "Input 17", id: "17" },
+          { 17: "Input 18", id: "18" },
+          { 18: "Input 19", id: "19" },
+          { 19: "Input 20", id: "20" },
+          {},
+        ],
+        outputLadels: {
+          0: "Output 1",
+          1: "Output 2",
+          2: "Output 3",
+          3: "Output 4",
+          4: "Output 5",
+          5: "Output 6",
+          6: "Output 7",
+          7: "Output 8",
+          8: "Output 9",
+          9: "Output 10",
+          10: "Output 11",
+          11: "Output 12",
+          12: "Output 13",
+          13: "Output 14",
+          14: "Output 15",
+          15: "Output 16",
+          16: "Output 17",
+          17: "Output 18",
+          18: "Output 19",
+          19: "Output 20",
+        },
+        videoOutputRouting: {
+          0: "0",
+          1: "1",
+          2: "2",
+          3: "3",
+          4: "4",
+          5: "5",
+          6: "6",
+          7: "7",
+          8: "8",
+          9: "9",
+          10: "10",
+          11: "11",
+          12: "12",
+          13: "13",
+          14: "14",
+          15: "15",
+          16: "16",
+          17: "17",
+          18: "18",
+          19: "19",
+        },
+      },
+
+      mode: "",
+
+      dst: "0",
+
+      src: "0",
     };
+  },
+  methods: {
+    //
+    onButtonClick() {},
+    onModeClick(id) {
+      if (id === "src") {
+        if (this.mode !== "src") {
+          this.mode = "src";
+        } else {
+          this.mode = "";
+        }
+      } else if (id === "dst") {
+        if (this.mode !== "dst") {
+          this.mode = "dst";
+        } else {
+          this.mode = "";
+        }
+      } else {
+        this.mode = "";
+      }
+    },
+  },
+  computed: {
+    activeNum() {
+      if (this.mode === "dst") {
+        return this.dst;
+      } else if (this.mode === "src") {
+        return this.config.videoOutputRouting[this.dst];
+      } else {
+        return "";
+      }
+    },
   },
 };
 </script>
@@ -154,3 +234,5 @@ export default {
   -moz-box-shadow: 1px -2px 143px 11px rgba(255, 255, 255, 0.77);
 }
 </style>
+
+// onClickBtn // เช็ค this.dst ก่อน ถ้าไม่ใช่ไม่ต้องทำอะไร // เช็ค this.src
